bf252229aed810978d44560a98a0eb75fefeca90
Bekhzod <bexzod.dev@gmail.com>
2024-04-01 08:10:58 +0500

    gunicorn ornatildi

--- core/settings.py
+++ core/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure--9t0pq+bq$&$u5etnucie!i_xaol9nm8j5lm_qz2j6d6#tvatr
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -128,8 +128,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
-# STATICFILES_DIRS = [BASE_DIR / 'static']
-# STATIC_ROOT = BASE_DIR / 'static-files'
+STATICFILES_DIRS = [BASE_DIR / 'static']
+STATIC_ROOT = BASE_DIR / 'static-files'
 
 MEDIA_URL = 'media/'
 MEDIA_ROOT = BASE_DIR / 'media-files'
